5d6b0567a067cb4cec086a378c8fbe5b4394fe8d
Samuel Janek <samueljanek17@gmail.com>
2022-08-09 15:41:14 +0200

    Add DbContext and generate initial migration

---
+++ WebApiPlugin.Identity.Sample/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiPlugin.Identity.Sample
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext() { }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(new[]
+            {
+                new IdentityRole()
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                }
+            });
+        }
+    }
+}
--- WebApiPlugin.Identity.Sample/Program.cs
+++ WebApiPlugin.Identity.Sample/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiPlugin.Identity.Extensions;
+
 namespace WebApiPlugin.Identity.Sample
 {
     public class Program
@@ -9,6 +12,16 @@ namespace WebApiPlugin.Identity.Sample
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            // Added
+            builder.Services.AddDbContext<AppDbContext>(options
+                => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Added
+            builder.Services.ConfigureIdentity<AppDbContext>(builder.Configuration, (identityOptions) =>
+            {
+                identityOptions.InitialRole = "Admin";
+            });
+
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
@@ -18,6 +31,11 @@ namespace WebApiPlugin.Identity.Sample
             }
 
             app.UseHttpsRedirection();
+
+            app.MapGet("api/data", () => Results.Ok(new List<string>() { "1", "2", "3" }));
+
+            // Added
+            app.MapEndpointsForIdentity();
 
             app.Run();
         }
